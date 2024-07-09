ff657ee9f756a7b7c19fd15843c586f8413ffd04
西野択 <nishino.t.ae@m.titech.ac.jp>
2024-07-09 13:42:45 +0900

    Classes and UI for saving Figure data and creating GIFs

---
+++ +UI/figCapture.m
@@ -0,0 +1,233 @@
+classdef figCapture < handle
+    properties(Dependent)
+        FileName
+        Extension
+        SavePath
+    end
+
+    properties(Access=protected)
+        uifig
+        uigrid
+        uiCapture
+
+        uiTextFolder
+        uiFolderIcon
+
+        uiExtensionIcon
+        uiTextExtension
+        uiFileName
+    end
+
+    methods
+        function obj = figCapture(varargin)
+
+            
+            p = inputParser;
+            p.CaseSensitive = false;
+            addParameter(p, 'Position' , [500,500,215,200]);
+            parse(p, varargin{:});
+            opt = p.Results;
+
+            
+            obj.uifig  = uifigure('Name','Figure Capture','Position',opt.Position);
+            obj.uigrid = uigridlayout(obj.uifig,[4 3]);
+
+
+            %%%%%%%%%%%%%%%%%%%%%%% build component %%%%%%%%%%%%%%%%%%%%%%%
+            impath = fileparts(mfilename('fullpath'));
+            obj.uiCapture = uibutton(obj.uigrid, ...
+                              'Text'    ,'capture',...
+                              'FontSize',18,...
+                              'Icon'    ,fullfile(impath,'image/camera.png'),...
+                              'IconAlignment','top',...
+                              'BackgroundColor',[0.8,0.8,0.8],...
+                              "ButtonPushedFcn", @(src,event) obj.capture...
+                               );
+            
+            obj.uiFolderIcon  = uibutton(obj.uigrid, ...
+                              'Text','',...
+                              'Icon'    ,fullfile(impath,'image/folder.png'),...
+                              'IconAlignment','center',...
+                              'BackgroundColor',[0.9,0.9,0.9],...
+                              "ButtonPushedFcn", @(src,event) obj.setPath...
+                               );
+            
+            obj.uiExtensionIcon = uibutton(obj.uigrid, ...
+                              'Text','',...
+                              'Icon'    ,fullfile(impath,'image/setting.png'),...
+                              'IconAlignment','center',...
+                              'BackgroundColor',[0.9,0.9,0.9],...
+                              "ButtonPushedFcn", @(src,event) obj.setExtenstion...
+                               );
+
+            obj.uiFileName     = uitextarea(obj.uigrid);
+            obj.uiTextFolder   = uilabel(obj.uigrid,'FontSize',8);
+            obj.uiTextExtension= uilabel(obj.uigrid,'FontSize',8);
+            
+            tag_file = uilabel(obj.uigrid,'Text','Name','FontSize',6);
+            tag_dir  = uilabel(obj.uigrid,'Text','Path','FontSize',6);
+            tag_ext  = uilabel(obj.uigrid,'Text','Ext' ,'FontSize',6);
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% UI LAYOUT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            obj.uigrid.RowHeight   = {80,'1x','1x','1x'};
+            obj.uigrid.ColumnWidth = {28,'1x',20};
+
+                                         % Caputure Botton
+                                         obj.uiCapture.Layout.Row      = 1;
+                                         obj.uiCapture.Layout.Column   = [1,3];
+
+            % Tag "Name"                 % textarea >> file name
+            tag_file.Layout.Row   = 2;   obj.uiFileName.Layout.Row     = 2;
+            tag_file.Layout.Column= 1;   obj.uiFileName.Layout.Column  = 2;
+
+            % Tag "Path"                 % textarea >> save path                % Folder Icon
+            tag_dir.Layout.Row    = 3;   obj.uiTextFolder.Layout.Row   = 3;     obj.uiFolderIcon.Layout.Row   = 3;
+            tag_dir.Layout.Column = 1;   obj.uiTextFolder.Layout.Column= 2;     obj.uiFolderIcon.Layout.Column= 3;
+
+            % Tag ".ext"                 % choice >> extension
+            tag_ext.Layout.Row    = 4;   obj.uiTextExtension.Layout.Row   = 4;  obj.uiExtensionIcon.Layout.Row   = 4;
+            tag_ext.Layout.Column = 1;   obj.uiTextExtension.Layout.Column= 2;  obj.uiExtensionIcon.Layout.Column= 3;
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            obj.FileName = [];
+            obj.SavePath = pwd;
+            obj.Extension = {'png','eps','fig'};
+        end
+
+
+        %%%%%% Set Method %%%%%%
+        function set.SavePath(obj,val)
+            if isempty(val)
+                val = uigetdir;
+                if ~val; error('Select Save Folder'); end
+            elseif ~isfolder(val)
+                error('It is not Folder')
+            end
+            obj.uiTextFolder.Text = val;
+        end
+
+        function set.FileName(obj,val)
+            if isempty(val)
+                val = "Fig"+string(datetime('today','Format','yyMMdd'));
+            end
+            obj.uiFileName.Value = val;
+        end
+
+        function set.Extension(obj,val)
+            if isempty(val)
+                obj.setterExtension();
+                return
+            elseif isstring(val)
+                val = arrayfun(@(i) {char(val(i))}, 1:numel(val),'UniformOutput',false);
+            elseif ischar(val)
+                val = {val};
+            elseif iscell(val)
+                val = cellfun(@(c) char(c), val, 'UniformOutput',false);
+            else
+                error('Format is wrong')
+            end
+            idx = ismember(val, {'jpeg','png','tiff','tiffn','meta','pdf','eps','epsc','eps2','epsc2','meta','svg','fig'});
+            cellfun(@(c) disp(['  >> removed:',c]), val(~idx));
+            val = val(idx);
+            if sum(ismember(val,{'tiff','tiffn'}))>1
+                warning('"tiff"と"tiffn"はどちらも.tif拡張子で保存されるため、いずれか１つを選択してください。')
+            end
+            if sum(ismember(val,{'eps','epsc','eps2','epsc2'}))>1
+                warning('"eps","epsc","eps2","epsc2"はいずれも.eps拡張子で保存されるため、いずれか１つを選択してください。')
+            end
+            text = cell(2,numel(val));
+            text(1,:) = val;
+            text(2,:) = {', '};
+            text(end) = {''};
+            obj.uiTextExtension.Text = horzcat(text{:});
+            obj.uiTextExtension.UserData  = val;
+        end
+        
+        %%%%%% Get Method %%%%%%
+        function val = get.SavePath(obj)
+            val = obj.uiTextFolder.Text;
+        end
+
+        function val = get.FileName(obj)
+            val = obj.uiFileName.Value;
+        end
+
+        function val = get.Extension(obj)
+            val = obj.uiTextExtension.UserData;
+        end
+    end
+
+    methods(Access=protected)
+
+        %%%%%%%%%%%%%%%% Callback Function %%%%%%%%%%%%%%%%
+        function capture(obj)
+            Tab = struct2table(dir(obj.SavePath));
+            [~,ExistFile(:),~] = cellfun(@(n) fileparts(n), Tab.name(~Tab.isdir), 'UniformOutput',false);
+            cnt = 1;
+            tempname = obj.FileName{1};
+            while ismember(tempname,ExistFile)
+                tempname = obj.FileName{1}+"("+cnt+")";
+                cnt = cnt+1;
+            end
+
+            f = gcf;
+            filepath = fullfile(obj.SavePath,tempname);
+            cellfun(@(e) saveas(f,filepath,e), obj.Extension)
+            disp(['>> Save Figure @',obj.SavePath])
+        end
+
+        function setPath(obj)
+            obj.SavePath = [];
+        end
+
+        function setExtenstion(obj)
+            obj.Extension = [];
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% UI APP for set extension %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function setterExtension(obj)
+            fig = uifigure('Name','Extension Setting','Position',[obj.uifig.Position(1:2),500,350]);
+            grd = uigridlayout(fig,[2 2]);
+
+            % tableの読み込み
+                tabpath = fileparts(mfilename('fullpath'));
+                data = readtable(fullfile(tabpath,'config/FigExtension.csv'));
+                if ~isempty(obj.Extension)
+                    data.set = ismember(data.val,obj.Extension);
+                else  
+                    data.set = logical(data.set);
+                end
+
+            % uitableの設定
+                tab = uitable(grd,"Data",data,'ColumnWidth',{'fit',40,55,'1x'});
+                tab.ColumnEditable = [true,false,false,false];
+
+            % ui buttonの設定
+                bot = uibutton(grd,'Text','Save','BackgroundColor',[0.8,0.8,0.8],...
+                                  "ButtonPushedFcn", @(src,event) SaveFcn(fig,tab,obj));
+
+            grd.RowHeight   = {'1x',35};
+            grd.ColumnWidth = {'1x',100};
+
+            tab.Layout.Row      = 1;
+            tab.Layout.Column   = [1,2];
+
+            bot.Layout.Row      = 2;
+            bot.Layout.Column   = 2;
+
+            function SaveFcn(fig,tab,cls)
+                cls.Extension = tab.Data.val(tab.Data.set);
+                delete(fig)
+            end
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    end
+end
+
---
+++ figProcessor.m
@@ -0,0 +1,168 @@
+% 自作Figureクラス
+% 使い方: コード冒頭で本クラスのインスタンスを定義、figure作成時には本クラスのfigureメソッドを使う、コードの最後でsaveメソッドを使う
+% is_save: figureを保存するかどうか
+% filename_save: 保存するファイル名(".../figure/{filename}"を想定, {filename}はmfilename()で取得)
+% type_save: 保存するファイルの拡張子(png, fig, jpg, both: png&fig)
+
+classdef figProcessor < handle
+    properties
+
+        figure
+        SavePath  = 0;
+
+        defaultFigFileName = "Fig"+string(datetime("now","Format","yyMMDD"));
+        defaultGifFileName = "Gif"+string(datetime("now","Format","yyMMDD"));
+        defaultFigExtension = {'fig','eps','png'};
+    end
+
+    properties(Access=protected)
+        GifName   = [];
+        GifFlag   = 'done'; % {'init',[],'done'}
+    end
+
+    methods
+        function obj = figProcessor(varargin)
+            if nargin>0 && isa(varargin{1},'matlab.ui.Figure')
+                obj.figure = varargin{1};
+            else
+                obj.figure   = figure( varargin{:} );
+            end
+
+            while ~obj.SavePath
+                disp('Choose Save Folder')
+                path = uigetdir;
+                if path
+                    obj.SavePath = path;
+                end
+            end
+        end
+
+        %%%%%%%%%%%%%%% save figure as GIF %%%%%%%%%%%%%%%
+
+        function getframe(obj,flag,filename)
+            arguments
+                obj
+                flag {mustBeMember(flag,["init","done","progress",[]])} = 'progress'
+                filename {mustBeText} = obj.defaultGifFileName
+            end
+            
+            switch flag
+                case 'init'     % gifのの１枚目作成の場合はimwriteへのoptionはなし
+                    name = obj.GenerateFilename('gif','FileName',filename);                    
+                    obj.GifName = name{1};
+                    obj.GifFlag = flag;
+                    return
+                case 'done'     % GifFlagが"終了(done)"になっている場合はrecordGifを実行する
+                    obj.GifFlag = flag;
+                    return
+                case 'progress' % gifのの２枚目以降の場合はimwriteのoptionとして'append'を指定
+                    switch obj.GifFlag
+                        case 'done'
+                            obj.getframe('init',filename)
+                            return
+                        case 'init'
+                            opt = {};
+                            obj.GifFlag = 'progress';
+                        case 'progress'
+                            opt = {'WriteMode','append'};
+                    end
+
+            end
+            drawnow
+            pause(0.001)
+            F = getframe(obj.figure);
+            [X,map] = rgb2ind(F.cdata,256);
+            imwrite(X,map,obj.GifName,opt{:});
+        end
+
+
+        %%%%%%%%%%%%%%% save figure as picture %%%%%%%%%%%%%%%
+        function save(obj, varargin)
+            filepath = obj.GenerateFilename('fig',varargin{:});
+            cellfun(@(name) saveas(obj.figure, name), filepath);
+        end
+
+
+
+        %%%%%%%%%%%%%%%%% Set Method %%%%%%%%%%%%%%%%%
+        function set.SavePath(obj,path)
+            obj.SavePath = check_SavePath(path);
+        end
+        function set.defaultFigExtension(obj,ext)
+            obj.defaultFigExtension = check_extension(ext);
+        end
+        function set.defaultFigFileName(obj,name)
+            obj.defaultFigFileName = check_FileName(name);
+        end
+        function set.defaultGifFileName(obj,name)
+            obj.defaultGifFileName = check_FileName(name);
+        end
+    end
+
+
+    methods(Access=protected)
+        function filepath = GenerateFilename(obj,mode,varargin)
+        % default parameter
+            p = inputParser;
+            p.CaseSensitive = false;
+            addParameter(p, 'SavePath' , obj.SavePath );
+            switch mode
+                case 'fig'
+                    addParameter(p, 'FileName' , obj.defaultFigFileName );
+                    addParameter(p, 'extension', obj.defaultFigExtension);
+                case 'gif'
+                    addParameter(p, 'FileName' , obj.defaultGifFileName );
+                    addParameter(p, 'extension', {'gif'} );
+            end
+            parse(p, varargin{:});
+            opt = p.Results;
+
+        % Check Parameter
+            if nargin>2
+                opt.extension = check_extension( opt.extension );
+                opt.SavePath  = check_SavePath(  opt.SavePath  );
+            end
+
+        % Check for duplicate file names
+
+            Tab = struct2table(dir(opt.SavePath));
+            [~,ExistFile(:),~] = cellfun(@(n) fileparts(n), Tab.name(~Tab.isdir), 'UniformOutput',false);
+
+            cnt = 1;
+            tempname = opt.FileName;
+
+            while ismember(tempname,ExistFile)
+                tempname = opt.FileName+"("+cnt+")";
+                cnt = cnt+1;
+            end
+
+        % export filepath
+            filename = fullfile(opt.SavePath,tempname);
+            filepath = cellfun(@(ext) filename+"."+ext, opt.extension, 'UniformOutput',false);
+        end
+    end
+end
+
+
+
+function ext = check_extension(ext)
+    format_list = {'fig','m','jpg','png','eps','pdf','tif','gif'};
+    if ( isstring(ext) || ischar(ext) ) && ismember(ext,format_list)
+        ext = {string(ext)};
+    elseif iscell(ext)
+        idx = ismember(ext,format_list);
+        cellfun(@(c) disp(['  >> 除外されました：',char(c)]), ext(~idx))
+        ext = ext(idx);
+    end
+end
+
+function path = check_SavePath(path)
+    if isempty(path)
+        path = uigetdir;
+    end
+    
+    if ~isfolder(path)
+        error('It is not folder.')
+    end
+end
+
